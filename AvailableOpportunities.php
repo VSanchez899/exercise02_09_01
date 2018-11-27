a5c10e7bb577316383572d23973582faeea2634f
Unknown <VSanchez899@west-mec.org>
2018-11-27 14:15:55 -0800

    done

--- AvailableOpportunities.php
+++ AvailableOpportunities.php
@@ -1,122 +1,146 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Available Opportunities</title>
+</head>
+
+<body>
     <h1>College Internship</h1>
     <h2>Available Opportunities</h2>
     <?php
+    
     if (isset($_REQUEST['internID'])) {
         $internID = $_REQUEST['internID'];
     } else {
         $internID = -1;
     }
+    //debug
     echo "\$internID: $internID\n";
+    
     $errors = 0;
     $hostname = "localhost";
     $username = "adminer";
     $passwd = "judge-quick-25";
-    $DBConnect = "false";
-    $DBName = "internships1";
+    $DBConnect = false;
+    $DBName = "internships1"; 
     if ($errors == 0) {
         $DBConnect = mysqli_connect($hostname, $username, $passwd);
         if (!$DBConnect) {
             ++$errors;
-            echo "<p style='text-align: center;'>unable to connect to database server error code: " . mysqli_connect_error() . "</p>\n";
+             echo "<p>Unable to connect to database server error code: " . mysqli_connect_error() . "</p>\n";
         }
         else {
             $result = mysqli_select_db($DBConnect, $DBName);
             if (!$result) {
                 ++$errors;
-                echo "<p style='text-align: center;'>unable to select the database \"$DBName\" error code: " . mysqli_connect_error() . "</p>\n";
-        }
+            echo "<p>Unable to select the database \"$DBName\" error code: " . mysqli_error($DBConnect) . "</p>\n";
+            } 
         }
     }
     $TableName = "interns";
     if ($errors == 0) {
-        $SQLstring = "SELECT * FROM $TableName" . 
-        " WHERE internID='$internID'";
+        $SQLstring = "SELECT * FROM $TableName" . " WHERE internID='$internID'";
         $queryResult = mysqli_query($DBConnect, $SQLstring);
         if (!$queryResult) {
             ++$errors;
             echo "<p>Unable to execute the query, error code: " . mysqli_errno($DBConnect) . ": " . mysqli_error($DBConnect) . "</p>\n";
-        }
+        } 
         else {
-            if (mysqli_num_rows($queryResult) == 0) {
-                ++$errors;
-                echo "<p>Invalid Intern ID!</p>";
-            }
+        if (mysqli_num_rows($queryResult) == 0) {
+            ++$errors;
+            echo "<p>Invalid Intern ID!</p>\n";
         }
+    }
     }
     if ($errors == 0) {
         $row = mysqli_fetch_assoc($queryResult);
-        $internName = $row['first'] . " " . $row['last'];
+        $internName = $row['first'] . " " . $row['last'];  
     }
     else {
         $internName = "";
     }
-    echo "<p>\$internName: $internName</p>\n";
+    echo "\$internName: $internName";
     $TableName = "assigned_opportunities";
+    
     if ($errors == 0) {
-        $SQLstring = "SELECT COUNT(opportunityID)" . 
-        " FROM $TableName" . 
-        " WHERE internID ='$internID'" . 
-        " AND dateApproved IS NOT NULL";
-        $queryResult = mysqli_query($DBConnect, $SQLstring);
+    $SQLstring = "SELECT COUNT(opportunityID)" . " FROM $TableName" . " WHERE internID='$internID'" . " AND dateApproved IS NOT NULL";
+    $queryResult = mysqli_query($DBConnect, $SQLstring);
         if (mysqli_num_rows($queryResult) > 0) {
-            $row = mysqli_fetch_row($queryResult);
-            $approvedOpportunities = $row[0];
-            mysqli_free_result($queryResult);
-        }
-        $selectedOpportunities = array();
-        $SQLstring = "SELECT opportunityID FROM $TableName" . 
-        " WHERE internID='$internID'";
+        $row = mysqli_fetch_row($queryResult);
+        $approvedOpportunities = $row[0];
+        mysqli_free_result($queryResult);
+    }
+    $selectedOpportunities = array();
+        $SQLstring = "SELECT opportunityID FROM $TableName" . " WHERE internID='$internID'";
         $queryResult = mysqli_query($DBConnect, $SQLstring);
         if (mysqli_num_rows($queryResult) > 0) {
             while (($row = mysqli_fetch_row($queryResult)) != false) {
-                $selectedOpportunities[] = $row[0];
+                $selectedOpportunities[] = $row[0]; 
             }
             mysqli_free_result($queryResult);
         }
         $assignedOpportunities = array();
-        $SQLstring = "SELECT opportunityID FROM $TableName" . 
-        " WHERE internID='$internID'";
+        $SQLstring = "SELECT opportunityID FROM $TableName" . " WHERE dateApproved IS NOT NULL";
         $queryResult = mysqli_query($DBConnect, $SQLstring);
-        if (mysqli_num_rows($queryResult) > 0) {
+    if (mysqli_num_rows($queryResult) > 0) {
             while (($row = mysqli_fetch_row($queryResult)) != false) {
-                $assignedOpportunities[] = $row[0];
+                $assignedOpportunities[] = $row[0]; 
             }
             mysqli_free_result($queryResult);
         }
-        $TableName = "opportunities";
-        $opportunities = array();
-        $SQLstring = "SELECT opportunityID, company, city," . 
-        " startDate, endDate, position, description" . 
+    $TableName = "opportunities";
+    $opportunities = array();
+    $SQLstring = "SELECT opportunityID, company, city," . " startDate, endDate, position, description" .
         " FROM $TableName";
-        $queryResult = mysqli_query($DBConnect, $SQLstring);
+    $queryResult = mysqli_query($DBConnect , $SQLstring);
         if (mysqli_num_rows($queryResult) > 0) {
             while (($row = mysqli_fetch_assoc($queryResult)) != false) {
-                $opportunities[] = $row;
+                $opportunities[] = $row; 
             }
             mysqli_free_result($queryResult);
-        }
-        echo "<pre>\n";
-        print_r($opportunities);
-        echo "</pre>\n";
-         
+        } 
     }
-
-
     if ($DBConnect) {
-        echo "<p style='text-align: center;'>closing Database \"$DBName\" connection.</p>\n";
-        mysqli_close($DBConnect);
+        echo "<p>Closing database \"$DBName\" connection.</p>\n";
+            mysqli_close($DBConnect);
+    }
+    echo "<table border='1' width='100%'>\n";
+    echo "<tr>\n";
+    echo "<th style='background-color: cyan'>Company</th>\n";
+    echo "<th style='background-color: cyan'>City</th>\n";
+    echo "<th style='background-color: cyan'>State</th>\n";
+    echo "<th style='background-color: cyan'>End Date</th>\n";
+    echo "<th style='background-color: cyan'>Position</th>\n";
+    echo "<th style='background-color: cyan'>Description</th>\n";
+    echo "<th style='background-color: cyan'>Status</th>\n";
+    echo "</tr>\n";
+    foreach ($opportunities as $opportunity) {
+        if (!in_array($opportunity['opportunityID'], $assignedOpportunities)) {
+            echo "<tr>\n";
+            echo "<td>" . htmlentities($opportunity['company']) . "</td>\n";
+            echo "<td>" . htmlentities($opportunity['city']) . "</td>\n";
+            echo "<td>" . htmlentities($opportunity['startDate']) . "</td>\n";
+            echo "<td>" . htmlentities($opportunity['endDate']) . "</td>\n";
+            echo "<td>" . htmlentities($opportunity['position']) . "</td>\n";
+            echo "<td>" . htmlentities($opportunity['description']) . "</td>\n";
+            echo "<td>\n";
+            if (in_array($opportunity['opportunityID'], $selectedOpportunities)) {
+                echo "Selected";
+            }
+            else if($approvedOpportunities > 0) {
+                echo "Open";
+            }
+            else {
+                echo "<a href='RequestOpportunity.php?" . "internID=$internID&" . "opportunityID=" . $opportunity['opportunityID'] . "'>Available</a>\n";
+            }
+            echo "</td>\n";
+            echo "</tr>\n";
         }
-        echo "<table border='1' width='100%'>\n";
-        echo "<p><a href='InternLogin.php'>Log Out</a></p>\n";
-        echo "</table>";
+    }
+    echo "</table>\n";
+    echo "<p><a href='internLogin.php'>Log Out</a></p>\n";
     ?>
-    </body>
+</body>
 </html>
--- VerifyLogin.php
+++ VerifyLogin.php
@@ -1,70 +1,76 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Verify Intern Login</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Verify Login</title>
+  <script src="modernizr.custom.65897.js"></script>
 </head>
-
+​
 <body>
-    <h1 style='text-align: center;'>College Internship</h1>
-    <h2 style='text-align: center;'>Verify Intern Login</h2>
-    <?php
-
-    $errors = 0;
-    $hostname = "localhost";
-    $username = "adminer";
-    $passwd = "judge-quick-25";
-    $DBConnect = "false";
-    $DBName = "internships1";
-    if ($errors == 0) {
-        $DBConnect = mysqli_connect($hostname, $username, $passwd);
-        if (!$DBConnect) {
-            ++$errors;
-            echo "<p style='text-align: center;'>unable to connect to database server error code: " . mysqli_connect_error() . "</p>\n";
-        }
-        else {
-            $result = mysqli_select_db($DBConnect, $DBName);
-            if (!$result) {
-                ++$errors;
-                echo "<p style='text-align: center;'>unable to select the database \"$DBName\" error code: " . mysqli_connect_error() . "</p>\n";
-        }
-        }
+  <h1>College Internship</h1>
+  <h2>Verify Intern Login</h2>
+  <?php
+  // global variables
+  $errors = 0;
+  $hostname = "localhost";
+  $username = "adminer";
+  $passwd = "judge-quick-25";
+  $DBConnect = false;
+  $DBName = "internships1"; 
+  // if there are no errors then it will connect
+  if ($errors == 0) {
+    $DBConnect = mysqli_connect($hostname, $username, $passwd);
+    // if there is no connection to the server then it will display an error 
+    if (!$DBConnect) {
+      ++$errors;
+       echo "<p>Unable to connect to database server error code: " . mysqli_connect_error() . "</p>\n";
     }
-    $TableName = "interns";
-    if ($errors == 0) {
-        $SQLstring = "SELECT internID, first, last" .
-        " FROM $TableName" . 
-        " WHERE email='" . stripslashes($_POST['email']) . 
-        "' AND password_md5='" . 
-        md5(stripslashes($_POST['password'])) . "'";
-        $queryResult = mysqli_query($DBConnect, $SQLstring);
-       if (mysqli_num_rows($queryResult) == 0) {
-           ++$errors;
-           echo "<p>The e-mail address password combination entered is not valid.</p>\n";
-       }
-       else {
-           $row = mysqli_fetch_assoc($queryResult);
-           $internID = $row['internID'];
-           $internName = $row['first'] . " " . $row['last'];
-           mysqli_free_result($queryResult);
-           echo "<p style='text-align: center;'>Welcome back, $internName!</p>\n";
-       }
+    else {
+      // this will select the database that we want to work on
+      $result = mysqli_select_db($DBConnect, $DBName);
+      // if there is no result to selecting the database then it will display an error
+      if (!$result) {
+        ++$errors;
+      echo "<p>Unable to select the database \"$DBName\" error code: " . mysqli_error($DBConnect) . "</p>\n";
+      } 
     }
-    if ($DBConnect) {
-        echo "<p style='text-align: center;'>closing Database \"$DBName\" connection.</p>\n";
-        mysqli_close($DBConnect);
-        echo "<form action='AvailableOpportunities.php' method='post'>\n";
-        echo "<input type='hidden' name='internID' value='$internID'>\n";
-        echo "<p style='text-align: center;'><input type='submit' name='submit' value='View Available Opportunities'></p>";
-        echo "</form>\n";
+  }
+  // this creates the table with the client info
+  $TableName = "interns";
+  if ($errors == 0) {
+    $SQLstring = "SELECT internID, first, last" . " FROM $TableName" . " WHERE email='" . stripslashes($_POST['email']) . "' AND password_md5='" . md5(stripslashes($_POST['password'])) . "'";
+    $queryResult = mysqli_query($DBConnect, $SQLstring);
+    // how many rows are in the query result and if there are no rows returned then we will increment our error and indicate an error that the password and/or username combination is not correct
+    if (mysqli_num_rows($queryResult) == 0) {
+      ++$errors;
+      echo "<p>The email address/password combination entered is not valid.";
     }
-    if ($errors > 0) {
-        echo "<p style='text-align: center;'>Please use your browser's BACK button to return to the form and fix the errors indicated</p>";
+    // this tells it to get a row from the query result (internID row and first/last name row)
+    else {
+      $row = mysqli_fetch_assoc($queryResult);
+      $internID = $row['internID'];
+      $internName = $row['first'] . " " . $row['last'];
+      //Fetch rows from a result-set, then free the memory associated with the result
+      mysqli_free_result($queryResult);
+      echo "<p>Welcome back, $internName!</p>\n";
     }
-
-    ?>
+  }
+  // if there still is a connection then it will close the connection if there are no errors
+  if ($DBConnect) {
+    echo "<p>Closing database \"$DBName\" connection.</p>\n";
+    mysqli_close($DBConnect);
+//    echo "<form action='AvailableOpportunities.php' method='post'>\n";
+//    // hidden form field hides the field from the user (web browser display)
+//    echo "<input type ='hidden' name='internID' value='$internID'>\n";
+//    echo "<input type='submit' name='submit' value='View Available Opportunities'>\n";
+//    echo "</form>\n";
+    echo "<p><a href='AvailableOpportunities.php?" . "internID=$internID'>Available Opportunities" . "</a></p>\n";
+  }
+  // indicates to go back to fix errors
+  if ($errors > 0) {
+    echo "Please use your browser's BACK button to return to the form and fix the errors indicated.";
+  }
+  ?>
 </body>
-
 </html>
