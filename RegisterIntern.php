0b9128460c10a3278c110042aaa588b267b1ece3
Unknown <VSanchez899@west-mec.org>
2018-11-28 14:26:05 -0800

    done for today

--- RegisterIntern.php
+++ RegisterIntern.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en" style="background-color: grey;">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
-    <h1 style="text-align: center;">College Intership</h1>
-    <h2 style="text-align: center;">Intern Registration</h2>
-    <?php
+<?php
+    $body = "";
     $errors = 0;
     $email = "";
     $hostname = "localhost";
@@ -25,14 +16,14 @@
         $email = stripslashes($_POST['email']);
         if (preg_match("/^[\w-]+(\.[\w-]+)*@[\w-]+(\.[w-]+)*(\.[A-Za-z]{2,})$/i", $email) == 0) {
             ++$errors;
-            echo "<p style='text-align: center;'>You need to enter a valid e-mail address.</p>\n";
+            $body .= "<p style='text-align: center;'>You need to enter a valid e-mail address.</p>\n";
             $email = "";
         }
         
     }
     if (empty($_POST['password'])) {
         ++$errors;
-        echo "<p style='text-align: center;'>You need to enter a password</p>\n";
+        $body .= "<p style='text-align: center;'>You need to enter a password</p>\n";
     }
     else{
         $password = stripslashes($_POST['password']);
@@ -40,7 +31,7 @@
         
     if (empty($_POST['password2'])) {
         ++$errors;
-        echo "<p style='text-align: center;'>You need to Confirm password</p>\n";
+        $body .= "<p style='text-align: center;'>You need to Confirm password</p>\n";
         }
     else{
         $password2 = stripslashes($_POST['password2']);
@@ -48,13 +39,13 @@
         if (!empty($password) && !empty($password2)) {
             if (strlen($password) < 6) {
                 ++$errors;
-                echo "<p style='text-align: center;'>The password is too short</p>\n";
+                $body .= "<p style='text-align: center;'>The password is too short</p>\n";
                 $password = "";
                 $password2 = "";
             }
             if ($password <> $password2) {
                 ++$errors;
-                echo "<p style='text-align: center;'>The password do not match</p>\n";
+                $body .= "<p style='text-align: center;'>The password do not match</p>\n";
                 $password = "";
                 $password2 = "";
             }
@@ -64,13 +55,13 @@
             $DBConnect = mysqli_connect($hostname, $username, $passwd);
             if (!$DBConnect) {
                 ++$errors;
-                echo "<p style='text-align: center;'>unable to connect to database server error code: " . mysqli_connect_error() . "</p>\n";
+                $body .= "<p style='text-align: center;'>unable to connect to database server error code: " . mysqli_connect_error() . "</p>\n";
             }
             else {
                 $result = mysqli_select_db($DBConnect, $DBName);
                 if (!$result) {
                     ++$errors;
-                    echo "<p style='text-align: center;'>unable to select the database \"$DBName\" error code: " . mysqli_connect_error() . "</p>\n";
+                    $body .= "<p style='text-align: center;'>unable to select the database \"$DBName\" error code: " . mysqli_connect_error() . "</p>\n";
                 } else {
                     
                 }
@@ -86,7 +77,7 @@
                     $row = mysqli_fetch_row($queryResult);
                     if ($row[0] > 0) {
                         ++$errors;
-                        echo "<p style='text-align: center;'>The e-mail address entered (" . htmlentities($email) . ") is already registered.</p>\n";
+                        $body .= "<p style='text-align: center;'>The e-mail address entered (" . htmlentities($email) . ") is already registered.</p>\n";
                     }
                 }
             }
@@ -100,7 +91,7 @@
                 $queryResult = mysqli_query($DBConnect, $SQLstring);
                 if (!$queryResult) {
                     ++$errors;
-                    echo "<p style='text-align: center;'>unable to save your registration error code: " . mysqli_error($DBConnect) . "</p>\n";
+                    $body .= "<p style='text-align: center;'>unable to save your registration error code: " . mysqli_error($DBConnect) . "</p>\n";
                 }
                 else {
                     $internID = mysqli_insert_id($DBConnect);
@@ -110,20 +101,35 @@
         }
         if ($errors == 0) {
             $internName = $first . " " . $last;
-            echo "<p style='text-align: center;'>Thank you, $internName. " . "Your new intern ID is <strong>" . 
+            $body .= "<p style='text-align: center;'>Thank you, $internName. " . "Your new intern ID is <strong>" . 
                 $internID . "</strong>.</p>\n";
         }
         if ($DBConnect) {
-            echo "<p style='text-align: center;'>closing Database \"$DBName\" connection.</p>\n";
+            setcookie("internID", $internID);
+            $body .= "<p style='text-align: center;'>closing Database \"$DBName\" connection.</p>\n";
             mysqli_close($DBConnect);
-            echo "<form action='AvailableOpportunities.php' method='post'>\n";
-            echo "<input type='hidden' name='internID' value='$internID'>\n";
-            echo "<p style='text-align: center;'><input type='submit' name='submit' value='View Available Opportunities'></p>";
-            echo "</form>\n";
+            $body .= "<form action='AvailableOpportunities.php' method='post'>\n";
+            $body .= "<input type='hidden' name='internID' value='$internID'>\n";
+            $body .= "<p style='text-align: center;'><input type='submit' name='submit' value='View Available Opportunities'></p>";
+            $body .= "</form>\n";
         }
         if ($errors > 0) {
-            echo "<p style='text-align: center;'>Please use your browser's BACK button to return to the form and fix the errors indicated</p>";
+            $body .= "<p style='text-align: center;'>Please use your browser's BACK button to return to the form and fix the errors indicated</p>";
         }
     ?>
+<!DOCTYPE html>
+<html lang="en" style="background-color: grey;">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+    <h1 style="text-align: center;">College Internship</h1>
+    <h2 style="text-align: center;">Intern Registration</h2>
+    <?php
+    echo "$body";
+    ?>
+    
     </body>
 </html>
---
+++ RequestOpportunity.php
@@ -0,0 +1,107 @@
+<?php
+$body = "";
+$errors = "0";
+$internID = "0";
+if (isset($_GET['internID'])) {
+    $internID = $_GET['internID'];
+} else {
+    ++$errors;
+    $body .= "<p>You have not logged in or registered</p>" . 
+    " please return to the  " . 
+    "<a href='InternLogin.php'>" . 
+    "Registration / Login Page</a></p>";
+}
+if ($errors == 0) {
+    if (isset($_GET['opportunityID'])) {
+        $opportunityID = $_GET['opportunityID'];
+    } else {
+        ++$errors;
+        $body .= "<p>You have not selected an opportunity</p>" . 
+        " please return to the  " . 
+        "<a href='AvailableOpportunity.php'>" . 
+        "Available Opportunity Page</a></p>";
+    }
+}
+$hostname = "localhost";
+  $username = "adminer";
+  $passwd = "judge-quick-25";
+  $DBConnect = false;
+  $DBName = "internships1"; 
+  // if there are no errors then it will connect
+  if ($errors == 0) {
+    $DBConnect = mysqli_connect($hostname, $username, $passwd);
+    // if there is no connection to the server then it will display an error 
+    if (!$DBConnect) {
+      ++$errors;
+      $body .= "<p>Unable to connect to database server error code: " . mysqli_connect_error() . "</p>\n";
+    }
+    else {
+      // this will select the database that we want to work on
+      $result = mysqli_select_db($DBConnect, $DBName);
+      // if there is no result to selecting the database then it will display an error
+      if (!$result) {
+        ++$errors;
+        $body .= "<p>Unable to select the database \"$DBName\" error code: " . mysqli_error($DBConnect) . "</p>\n";
+      } 
+    }
+  }
+  $displayDate = date("l, F j, Y, g:i A");
+  $body .= "\$displayDate: $displayDate<br>";
+  $dbDate = date("Y-m-d H:i:s");
+  $body .= "\$dbDate: $dbDate<br>";
+  if ($errors == 0) {
+      $tableName = "assigned_opportunities";
+      $SQLstring = "INSERT INTO $tableName" . 
+      " (opportunityID, internID, dateSelected)" . 
+      " VALUES($opportunityID, $internID, '$dbDate')";
+      $queryResult = mysqli_query($DBConnect, $SQLstring);
+      if (!$queryResult) {
+          ++$errors;
+          $body .= "<p>Unable to execute the query, " . 
+          "error code: " . mysqli_errno($DBConnect) . ": " .
+          mysqli_error($DBConnect) .
+          "</p>\n";
+      }
+      else {
+          $body .= "<p>Your results for opportunity #" . 
+          " $opportunityID have been entered on" . 
+          " $displayDate" . "</p>\n";
+      }
+  }
+  if ($DBConnect) {
+    $body .= "<p>Closing database \"$DBName\" connection.</p>\n";
+    mysqli_close($DBConnect);
+  }
+  if ($internID > 0) {
+      $body .= "<p>return to the " . 
+      "<a href='AvailableOpportunities.php?" . 
+      "internID=$internID'>Available Opportunities" . 
+      "</a> page.</p>\n";
+  }
+  else {
+      $body .= "<p>Please" .
+      "<a href='InternLogin.php'>" .
+      "Register or Log in" . 
+      "</a> to use this page.</p>\n";
+  }
+  if ($errors == 0) {
+      echo "Setting Cookie<br>";
+      setcookie("LastRequestDate", urlencode($displayDate), time()+60*60*24*7);
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>College Intern</h1>
+    <h2>Opportunity Requested</h2>
+    <?php
+    echo $body;
+    ?>
+</body>
+</html>
